0ce45529c91b4b33965de25b140c37e37ef57d6f
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-29 20:02:36 -0700

    Create gcsvh.sh

---
+++ gcsvh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while IFS='=' read -r -d '' n v; do
+    printf '  %s: "%s"\n' "$n" "$v" >> app.yaml
+done < <(env -0)
+
+sed -i '/^  HOSTNAME:/d' app.yaml 
+sed -i '/^  _:/d' app.yaml 
+sed -i '/^  BUILDER_OUTPUT:/d' app.yaml 
+sed -i '/^  PWD:/d' app.yaml 
+sed -i '/^  HOME:/d' app.yaml 
+sed -i '/^  SHLVL:/d' app.yaml 
+sed -i '/^  PATH:/d' app.yaml 
