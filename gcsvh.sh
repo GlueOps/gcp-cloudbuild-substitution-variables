5936982cd447f539b9ac62f98952b75ae1687fc4
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-06-20 20:16:56 -0700

    escaping \ in bash since app engine seems to not like it upon deployment

--- gcsvh.sh
+++ gcsvh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while IFS='=' read -r -d '' n v; do
-    printf '  %s: "%s"\n' "$n" "$v" >> app.yaml
+    printf '  %s: "%s"\n' "$n" "$v" | sed 's/\\/\\\\/g' >> app.yaml
 done < <(env -0)
 
 sed -i '/^  HOSTNAME:/d' app.yaml 
@@ -13,6 +13,3 @@ sed -i '/^  SHLVL:/d' app.yaml
 sed -i '/^  PATH:/d' app.yaml 
 sed -i '/^  XTERM:/d' app.yaml
 sed -i '/^  LS_COLORS:/d' app.yaml
-
-
-cat app.yaml
